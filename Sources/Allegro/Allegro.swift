bcf47cb6329ed7269eb2a21dac94879142d3b64d
SwiftStudies <redwhenexcited@icloud.com>
2018-10-25 13:29:37 +0100

    Added a constant to the shim to see if that can be seen

--- Sources/Allegro/Allegro.swift
+++ Sources/Allegro/Allegro.swift
@@ -1,6 +1,9 @@
 import CAllegro
 
 public enum Allegro {
+    public static var version : String {
+        return String(cString:_cAllegroVersion)
+    }
     public static func initialize()->Bool{
         return al_init()
     }
--- Sources/Callegro/CAllegro.h
+++ Sources/Callegro/CAllegro.h
@@ -1,1 +1,3 @@
 #include "allegro5/allegro.h"
+
+const char *_cAllegroVersion = "5.2.4";
